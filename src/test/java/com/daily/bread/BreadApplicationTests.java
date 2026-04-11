b0a70f60039e1c71557ed4851109353ce6afe21e
nosremehj <jhemerson1931@gmail.com>
2026-04-11 00:28:19 -0300

    first commit

---
+++ src/test/java/com/daily/bread/BreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daily.bread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
